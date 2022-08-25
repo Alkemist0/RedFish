e0e4dd3e2fe9475879611ffb5da4121f266b4bdb
EmeraldAlkemist <50426046+EmeraldAlkemist@users.noreply.github.com>
2022-08-25 15:54:51 -0700

    Added Fish Mechanics

---
+++ Assets/Scripts/Fish.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+
+public class Fish : MonoBehaviour
+{
+    public bool isRed;
+
+    GameObject redFish;
+    private GameObject target;
+
+    private bool reachedDestination = true;
+
+    private Vector2 endPos;
+    private Vector2 startPos;
+    private float desiredLength = 2f;
+    private float elapsedTime;
+
+    [SerializeField]
+    private AnimationCurve curve;
+
+    private void Awake()
+    {
+        redFish = GameObject.FindGameObjectWithTag("RedFish");
+
+        if(!isRed)
+        {
+            for (int i = 0; i < GameObject.FindGameObjectsWithTag("Target").Length; i++)
+            {
+                if (GameObject.FindGameObjectsWithTag("Target")[i].name == gameObject.name) target = GameObject.FindGameObjectsWithTag("Target")[i];
+            }
+
+            Debug.Log(target);
+        }
+    }
+
+    private void Update()
+    {
+        if(reachedDestination)
+        {
+            startPos = transform.position;
+            endPos = target.transform.position;
+            elapsedTime = 0f;
+        } else
+        {
+            transform.right = endPos - startPos;
+        }
+
+        if (new Vector2(transform.position.x, transform.position.y) == endPos)
+        {
+            reachedDestination = true;
+        }
+        else
+        {
+            reachedDestination = false;
+        }
+
+        elapsedTime += Time.deltaTime;
+        float percentageComplete = elapsedTime / desiredLength;
+
+        transform.position = Vector2.Lerp(startPos, endPos, curve.Evaluate(percentageComplete));
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/FollowMouse.cs
+++ Assets/Scripts/FollowMouse.cs
@@ -10,9 +10,17 @@ public class FollowMouse : MonoBehaviour
     [SerializeField]
     private AnimationCurve curve;
 
+    public bool reachedDestination = true;
+
+    private void Start()
+    {
+        startPos = transform.position;
+        endPos = transform.position;
+    }
+
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && reachedDestination)
         {
             startPos = transform.position;
             endPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -20,9 +28,22 @@ public class FollowMouse : MonoBehaviour
             transform.right = endPos - startPos;
         }
 
+        if(new Vector2(transform.position.x, transform.position.y) == endPos)
+        {
+            reachedDestination = true;
+        } else
+        {
+            reachedDestination = false;
+        }
+
         elapsedTime += Time.deltaTime;
         float percentageComplete = elapsedTime / desiredLength;
 
         transform.position = Vector2.Lerp(startPos, endPos, curve.Evaluate(percentageComplete));
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
     }
 }
